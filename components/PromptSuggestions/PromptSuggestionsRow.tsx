9c21f38079bbf089dcd3bd72a2bf0b2d918eb896
Waseem Raza <waseemraza3210@gmail.com>
2025-02-03 08:59:46 +0500

    Add files via upload

---
+++ components/PromptSuggestions/PromptSuggestionsRow.tsx
@@ -0,0 +1,17 @@
+import PromptSuggestionButton from "./PromptSuggestionButton";
+
+const PromptSuggestionRow = ({ onPromptClick }) => {
+  const prompts = [
+    'Hi there, I need survey form.',
+  ];
+
+  return (
+    <div className="flex flex-row flex-wrap justify-start items-center py-4 gap-2">
+      {prompts.map((prompt, index) => (
+        <PromptSuggestionButton key={`suggestion-${index}`} text={prompt} onClick={() => onPromptClick(prompt)} />
+      ))}
+    </div>
+  );
+};
+
+export default PromptSuggestionRow;
