321b049cfad68c055860bdc97005840dc28f6feb
Waseem Raza <waseemraza3210@gmail.com>
2025-02-03 09:00:36 +0500

    Add files via upload

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+
+const Footer = () => {
+  return (
+    <footer className="chatbot-text-tertiary flex justify-between text-sm mt-6">
+   
+      <div className="ml-auto flex flex-row items-center">
+      
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
